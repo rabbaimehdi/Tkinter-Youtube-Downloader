110fbd0f3057e3353e436ace18a07c24478b2a06
Mehdi <mehdi.rabbai@gmail.com>
2023-12-07 21:32:54 +0100

    Add Settings and App frame

--- main.py
+++ main.py
@@ -2,3 +2,21 @@
 import tkinter
 import customtkinter
 from pytube import YouTube
+
+#Settings
+
+customtkinter.set_appearance_mode("System")
+customtkinter.set_default_color_theme("blue")
+
+#App Frame
+app = customtkinter.CTk()
+app.geometry("720x480")
+app.title("Youtube Downloader")
+
+#App widgets
+Title = customtkinter.CTkLabel(app,text="Insert a youtube link")
+Title.pack()
+
+#Run App
+
+app.mainloop()
