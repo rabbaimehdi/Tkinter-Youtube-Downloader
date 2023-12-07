a867c5a4669085cbc4bb14e8794f72460b2c08f6
Mehdi <mehdi.rabbai@gmail.com>
2023-12-07 21:49:56 +0100

    Add Widgets and Function to Download videos

--- main.py
+++ main.py
@@ -3,9 +3,19 @@ import tkinter
 import customtkinter
 from pytube import YouTube
 
+#Define download function
+def start_download():
+    try:
+        yt_link = link.get()
+        yt_object = YouTube(yt_link)
+        video = yt_object.streams.get_highest_resolution()
+        video.download()
+        print("Donwload Complete!")
+    except:
+        print("Error : Link Invalid")
 #Settings
 
-customtkinter.set_appearance_mode("System")
+customtkinter.set_appearance_mode("Dark")
 customtkinter.set_default_color_theme("blue")
 
 #App Frame
@@ -14,8 +24,18 @@ app.geometry("720x480")
 app.title("Youtube Downloader")
 
 #App widgets
-Title = customtkinter.CTkLabel(app,text="Insert a youtube link")
-Title.pack()
+title = customtkinter.CTkLabel(app,text="Insert a youtube link")
+title.pack(padx = 10, pady = 10)
+
+#Link Input
+url = tkinter.StringVar()
+link = customtkinter.CTkEntry(app,width= 350, height=30, placeholder_text="Paste youtube link here", textvariable=url)
+link.pack()
+
+#Download Button
+
+download_button = customtkinter.CTkButton(app, width=140, height=28, text="Download", command=start_download)
+download_button.pack(padx = 10, pady = 10)
 
 #Run App
 
