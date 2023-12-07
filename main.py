d8ec254329eef38faa404c220cffef318e920121
Mehdi <mehdi.rabbai@gmail.com>
2023-12-07 22:21:39 +0100

    Add label to show download status

--- main.py
+++ main.py
@@ -7,12 +7,16 @@ from pytube import YouTube
 def start_download():
     try:
         yt_link = link.get()
+        print(yt_link)
         yt_object = YouTube(yt_link)
         video = yt_object.streams.get_highest_resolution()
         video.download()
-        print("Donwload Complete!")
+        
+        # print("Donwload Complete!")
+        finished_label.configure(text="Donwloaded!", text_color = 'green')
     except:
-        print("Error : Link Invalid")
+        # print("Error : Link Invalid")
+        finished_label.configure(text="Error : Link Invalid!", text_color = "red")
 #Settings
 
 customtkinter.set_appearance_mode("Dark")
@@ -31,6 +35,11 @@ title.pack(padx = 10, pady = 10)
 url = tkinter.StringVar()
 link = customtkinter.CTkEntry(app,width= 350, height=30, placeholder_text="Paste youtube link here", textvariable=url)
 link.pack()
+
+#Video title after download
+
+finished_label = customtkinter.CTkLabel(app, text="")
+finished_label.pack(padx = 5, pady = 5)
 
 #Download Button
 
